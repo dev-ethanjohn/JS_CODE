3133f899dbb14e35b0e6ccb92f26b10ed48d4550
dev-ethanjohn <dev.ejohnpaguntalan@gmail.com>
2023-09-15 06:59:00 -0700

    Add Conditional Statement Lesson

---
+++ 005-conditional-statements/lesson/if-else-if-statement.js
@@ -0,0 +1,11 @@
+let health = 20;
+
+if (health > 50) {
+  console.log("You have more than 50% health!");
+} else if (health > 20) {
+  console.log("You are running low on health!");
+} else {
+  console.log("You have less than or equal to 20 health!"); // (this will run)
+}
+
+console.log("Game Over!"); // run
---
+++ 005-conditional-statements/lesson/if-statement.js
@@ -0,0 +1,6 @@
+let points = 10;
+if (points > 5) {
+  console.log("You have more than 5 points"); // (will run)
+  console.log("Nice!"); //(will run)
+}
+console.log("All done!"); // (will run) whatever the boolean expression is true or false cuz it is independent from the condition
