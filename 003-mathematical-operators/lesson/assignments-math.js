9bb765af1f57acc225c404cea671d9e199e431f4
dev-ethanjohn <dev.ejohnpaguntalan@gmail.com>
2023-09-15 00:16:33 -0700

    Add math operators lesson

---
+++ 003-mathematical-operators/lesson/assignments-math.js
@@ -0,0 +1,29 @@
+// JS Mathematical Assignment Operators
+let points = 10;
+points++; // points = points + 1;
+console.log(points); // 11
+
+let score = 20;
+score--;
+score--;
+console.log(score); // 18
+
+score += 6;
+console.log(score); // 24
+
+score -= 50;
+console.log(score); // -26
+
+let age = 7;
+age *= 2; // age = age * 2;
+console.log(age); // 14
+
+age /= 2;
+console.log(age); // 7
+
+// age = 7
+age %= 7; // age = age % 7;
+console.log(age); // 0
+
+age **= 2;
+console.log(age); // 0
---
+++ 003-mathematical-operators/lesson/math-operators.js
@@ -0,0 +1,31 @@
+// Addition
+
+let age = 10 + 23;
+console.log(age); // 33
+
+// Subtraction
+
+let points = 50 - 19;
+console.log(points); // 31
+
+// Multiplication
+let score = 7 * 50;
+console.log(score); // 350
+
+// Division
+let divide = 100 / 10;
+console.log(divide); // 10
+
+// Modulus (Remainder)
+let mod = 6 % 3;
+console.log(mod); // 0
+
+mod = 7 % 3;
+console.log(mod); // 1
+
+// Exponentiation
+let exp = 4 ** 2;
+console.log(exp); // 16
+
+exp = exp ** 2;
+console.log(exp); // 256
