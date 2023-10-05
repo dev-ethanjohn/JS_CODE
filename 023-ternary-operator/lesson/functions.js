7e40fa6f063a12483e5d1a5e1d30889e1cd995bb
dev-ethanjohn <dev.ejohnpaguntalan@gmail.com>
2023-10-05 07:33:58 -0700

    Add lesson on ternary operator

---
+++ 023-ternary-operator/lesson/functions.js
@@ -0,0 +1,33 @@
+// Using if/else statement
+const happyCheck = (happy) => {
+  if (happy === true) {
+    return "Yay!";
+  } else {
+    return "Nope";
+  }
+};
+
+const result1 = happyCheck(true);
+const result2 = happyCheck(false);
+
+console.log(result1, result2); // Yay! Nope
+
+// Using Ternary operator
+const happyCheck2 = (happy) => {
+  // return "Yay!"
+  // return "Nope"
+  return happy === true ? "Yay!" : "Nope";
+};
+
+const result3 = happyCheck2(true);
+const result4 = happyCheck2(false);
+
+console.log(result3, result4); // Yay! Nope
+
+// SHORT version Ternary Operator
+const happyCheck3 = (happy) => (happy === true ? "Yay!" : "Nope");
+
+const result5 = happyCheck3(true);
+const result6 = happyCheck3(false);
+
+console.log(result5, result6); // // Yay! Nope
